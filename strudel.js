cd5828cff74c131011063a8d31d85d1c1951c365
Hbergamo <hbergamo@gmail.com>
2026-06-20 00:04:12 -0300

    Update strudel.js

--- strudel.js
+++ strudel.js
@@ -1,5 +1,5 @@
 {
-  "_base": "https://githubusercontent.com",
+  "_base": "https://raw.githubusercontent.com/1CA676C/media/main/strudel.json",
   "metro": ["som/metro/mm_clave1.wav"],
   "snare": ["som/perc/snare1.mp3"]
 }
